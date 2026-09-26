4d509af0764e8eb4c59985c3bcb0766f53d90b81
Atoyyyyyy <your-github-email@example.com>
2026-09-26 22:27:01 +0800

    Configure initial Wokwi simulation

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,42 @@
-#include "stm32f1xx_hal.h"
+#include "stm32f1xx.h"
 
-static void SystemClock_Config(void);
-
-void app_main(void)
+static void UART_SendChar(char c)
 {
-    for (;;)
+    while (!(USART1->SR & USART_SR_TXE)) { }
+    USART1->DR = c;
+}
+
+static void UART_SendString(const char *s)
+{
+    while (*s)
     {
-        HAL_Delay(1000);
+        UART_SendChar(*s++);
     }
 }
 
 int main(void)
 {
-    HAL_Init();
-    SystemClock_Config();
+    /* Enable clocks: GPIOA, AFIO, USART1 */
+    RCC->APB2ENR |= RCC_APB2ENR_IOPAEN | RCC_APB2ENR_AFIOEN | RCC_APB2ENR_USART1EN;
 
-    app_main();
+    /* PA9 = TX: alternate function push-pull, 50MHz (CNF=10, MODE=11) */
+    GPIOA->CRH &= ~(0xF << 4);
+    GPIOA->CRH |=  (0xB << 4);
 
-    return 0;
-}
+    /* PA10 = RX: floating input (CNF=01, MODE=00) */
+    GPIOA->CRH &= ~(0xF << 8);
+    GPIOA->CRH |=  (0x4 << 8);
 
-static void SystemClock_Config(void)
-{
+    /* USART1 at 115200 baud, assuming 8MHz default HSI clock:
+     * BRR = Fck / baud = 8000000 / 115200 ≈ 69.44 -> 0x45 (mantissa=4, fraction=5) */
+    USART1->BRR = 0x45;
+    USART1->CR1 = USART_CR1_UE | USART_CR1_TE | USART_CR1_RE;
+
+    for (;;)
+    {
+        UART_SendString("BCA182 FreeRTOS Multisensor\r\n");
+        UART_SendString("System starting...\r\n");
+
+        for (volatile int i = 0; i < 1000000; i++) { }
+    }
 }
